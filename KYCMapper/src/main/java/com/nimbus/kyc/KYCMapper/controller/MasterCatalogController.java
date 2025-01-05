4c168f7197f1ddb7cceeb9fb3857f246cefba900
juantorresdev <juan.torres.dev@gmail.com>
2025-01-04 23:00:46 -0600

    Added CatalogElementDTO class

--- KYCMapper/src/main/java/com/nimbus/kyc/KYCMapper/controller/MasterCatalogController.java
+++ KYCMapper/src/main/java/com/nimbus/kyc/KYCMapper/controller/MasterCatalogController.java
@@ -1,5 +1,6 @@
 package com.nimbus.kyc.KYCMapper.controller;
 
+import com.nimbus.kyc.KYCMapper.dto.CatalogElementDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import java.util.List;
 public class MasterCatalogController {
 
     @GetMapping("/element/{id}")
-    public ResponseEntity<void> getCatalogElement(@RequestParam("id") int value){
+    public ResponseEntity<CatalogElementDTO> getCatalogElement(@RequestParam("id") int value){
 
         return ResponseEntity.ok().build();
 
