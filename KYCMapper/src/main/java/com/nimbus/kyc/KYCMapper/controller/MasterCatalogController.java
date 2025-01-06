8083a823e5f8cce090a704dc09c9d6dad668adb6
juantorresdev <juan.torres.dev@gmail.com>
2025-01-05 23:06:18 -0600

    Confige logs xml

--- KYCMapper/src/main/java/com/nimbus/kyc/KYCMapper/controller/MasterCatalogController.java
+++ KYCMapper/src/main/java/com/nimbus/kyc/KYCMapper/controller/MasterCatalogController.java
@@ -1,6 +1,8 @@
 package com.nimbus.kyc.KYCMapper.controller;
 
 import com.nimbus.kyc.KYCMapper.dto.CatalogElementDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +14,12 @@ import java.util.List;
 @RestController
 public class MasterCatalogController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MasterCatalogController.class);
+
     @GetMapping("/element/{id}")
     public ResponseEntity<CatalogElementDTO> getCatalogElement(@RequestParam("id") int value){
 
+        logger.info("getCatalogElement");
         return ResponseEntity.ok().build();
 
     }
@@ -22,6 +27,11 @@ public class MasterCatalogController {
     @GetMapping("/getElements")
     public ResponseEntity<List<String>> getCatalogElements(){
 
+        logger.info("getCatalogElements");
+        logger.debug("getCatalogElements");
+        logger.error("getCatalogElements");
+        logger.warn("getCatalogElements");
+        logger.trace("getCatalogElements");
         return ResponseEntity.ok().body(new ArrayList<String>());
 
     }
